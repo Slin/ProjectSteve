f54f68c1e48bad0f8a55ef7245c5d739f82ada96
Thanduriel <rojendersie@alice.de>
2020-02-01 10:30:10 +0100

    + some git ignore entries

--- Assets/Shaders/CompileShaders.bat
+++ Assets/Shaders/CompileShaders.bat
@@ -1,2 +1,2 @@
 cd /D "%~dp0"
-python ../../../../Rayne/Tools/ShaderProcessor/convert.py Shaders.json cso,spirv,metal ../../Resources/shaders shaders
+python ../../../../Rayne/Tools/ShaderProcessor/convert.py Shaders.json cso ../../Resources/shaders shaders
