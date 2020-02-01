673721d467f6f0873ce186b61732b9f4c18c4994
Julian Benda <Zabuza98@gmx.net>
2020-02-01 23:57:16 +0100

    Aded genCharackterSheet scriept for sheet with all characters

---
+++ Assets/Sprites/Stevelette/genCharackterSheet.py
@@ -0,0 +1,21 @@
+from PIL import Image as img
+import os
+import math
+
+files = sorted(filter(lambda str: str[-4:] == ".png" , os.listdir('combo')))
+
+z = float(len(files))
+y = math.sqrt(z) * 3. / 4.
+x = y * 16. / 9.
+
+x = int(x)
+y = int(y)
+
+
+
+out = img.new('RGBA', (x * 128, y * 128 ))
+for i in range(x):
+	for j in range(y):
+		image = img.open('combo/' + files[i * y + j]).crop((0,0,128,128));
+		out.paste(image, (i * 128, j * 128, (i+1) * 128, (j+1)*128));
+out.save("out.png");
